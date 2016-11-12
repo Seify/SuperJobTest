bced9ff12da915e237cfa507450e07cd2452128b
ROMAN SMIRNOV <smirnov@pappico.ru>
2016-11-12 11:50:45 +0300

    Initial Commit

---
+++ SmirnovSuperJobTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmirnovSuperJobTest
+//
+//  Created by Roman S on 12.11.16.
+//  Copyright © 2016 RomanS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmirnovSuperJobTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmirnovSuperJobTest
+//
+//  Created by Roman S on 12.11.16.
+//  Copyright © 2016 RomanS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
